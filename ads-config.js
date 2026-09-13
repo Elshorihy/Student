af89fd12b7909726e112d08416cce200c7b43826
Elshorihy <sheenomatp@gmail.com>
2026-09-13 23:51:50 +0300

    chore: add safe AdSense configuration

---
+++ ads-config.js
@@ -0,0 +1,11 @@
+/*
+ * Google AdSense configuration.
+ * Leave ADSENSE_CLIENT empty until Google approves the site and gives you a
+ * publisher ID (ca-pub-...). Do not put a personal AdSense ID here before then.
+ */
+window.OMNITRACK_ADS = {
+  ADSENSE_CLIENT: '',
+  ENABLED: false,
+  TOP_SLOT: '',
+  BOTTOM_SLOT: ''
+};
